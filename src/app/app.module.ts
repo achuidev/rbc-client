f58374650c4959188344c7c6c00029b71f278db5
Akshay Chandwani <akshaychandwani@MacBook-Pro.local>
2024-02-11 18:18:35 -0500

    adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './components/hello-world/hello-world.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HelloWorldComponent],
@@ -13,6 +14,7 @@ import { HelloWorldComponent } from './components/hello-world/hello-world.compon
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/utils/app.constants.ts
+++ src/app/utils/app.constants.ts
@@ -1,3 +1,3 @@
-import { environment } from 'src/environment/environment';
-
 export const CONTEXT_ROOT = 'http://localhost:3000/v1/api';
+
+// export const CONTEXT_ROOT = 'http://localhost:3001/v1/api';
